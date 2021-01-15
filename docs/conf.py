bf6b4cd2c633d845f4b7a83a3a785e2488e1c2e7
Edmund Widl <edmund.widl@ait.ac.at>
2021-01-15 17:32:31 +0100

    fixed docs config for manual page

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'aitlablinkconfigurationserver', 'AIT Lablink Examples',
+    (master_doc, 'aitlablinkexamples', 'AIT Lablink Examples',
      [author], 1)
 ]
 
